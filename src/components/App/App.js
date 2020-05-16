1ee3b74cfb42f00903b87050de75d2b0534f9d0f
Srikar Reddy <srikarreddymadaka@gmail.com>
2020-05-16 15:25:29 +0530

    completed part 1 of the project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
-import logo from '../../logo.svg';
 
 class App extends React.Component{
   render(){
